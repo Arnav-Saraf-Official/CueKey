773e06fbe560b63205424b8e2a9eb8e6e9948fd0
arnav-saraf-official <reacharnavsaraf@gmail.com>
2026-07-15 12:52:46 -0500

    Change to static

--- electron/preload.cjs
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron')
-
-// Expose any Electron APIs to the renderer process here if needed
-contextBridge.exposeInMainWorld('electron', {
-  isElectron: true,
-})
